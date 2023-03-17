44170bd8f31e54bbd191a8cdd42bea38722a6597
EmmanuelR12 <developer7@tecnolika.com>
2023-03-17 17:21:50 -0600

    shortcuts telescope

--- lua/maps.lua
+++ lua/maps.lua
@@ -5,3 +5,19 @@ end
 -- NeoTree
 map("n", "<C-b>", "<CMD>Neotree toggle<CR>")
 --map("n", "<leader>o", "<CMD>Neotree focus<CR>")
+
+
+--Telescope
+-- sudo apt-get install ripgrep  necesario para busqueda de cadenas
+local status, telescope = pcall(require, "telescope.builtin")
+if status then
+    -- Telescope
+    map("n", "<C-p>", telescope.find_files)
+    map("n", "<leader>lm", telescope.live_grep)
+    map("n", "<leader>b", telescope.buffers)
+    map("n", "<leader>fh", telescope.help_tags)
+    map("n", "<leader>fs", telescope.git_status)
+    map("n", "<leader>fc", telescope.git_commits)
+else
+    print("Telescope not found")
+end
